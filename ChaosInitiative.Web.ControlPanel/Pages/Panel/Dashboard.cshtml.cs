c8e45e81916e7283e41d9cb7719cc9c93a41cdbd
Tewan <tewan@spycesoftware.com>
2020-09-23 00:06:47 +0200

    (feat) Readd dashboard model

---
+++ ChaosInitiative.Web.ControlPanel/Pages/Panel/Dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChaosInitiative.Web.ControlPanel.Pages.Panel
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
