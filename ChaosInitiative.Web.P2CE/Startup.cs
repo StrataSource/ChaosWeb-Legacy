a875242a5b8c9a6571ff20a10ada9476f0c01de8
Tewan <tewan@spycesoftware.com>
2020-10-09 15:57:04 +0200

    (feat) Use razor runtime compilation for p2ce

--- ChaosInitiative.Web.P2CE/Startup.cs
+++ ChaosInitiative.Web.P2CE/Startup.cs
@@ -23,7 +23,7 @@ namespace ChaosInitiative.Web.P2CE
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddControllers();
         }
 
