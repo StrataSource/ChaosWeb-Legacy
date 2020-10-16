d42f5ed72735cd7fcc2739d5b1f39dedd3207a10
Tewan <tewan@spycesoftware.com>
2020-10-16 16:04:50 +0200

    (feat) Add function to get full issue path

--- ChaosInitiative.Web.ControlPanel/Model/Game.cs
+++ ChaosInitiative.Web.ControlPanel/Model/Game.cs
@@ -3,6 +3,19 @@ namespace ChaosInitiative.Web.ControlPanel.Model
     public class Game
     {
         public string Name { get; set; }
-        public string Repository { get; set; }
+        private string _repository;
+
+        public string Repository
+        {
+            get => _repository;
+            set
+            {
+                // Strip trailing slashes
+                if (value.EndsWith("/"))
+                    value = value.Substring(0, value.Length - 1);
+
+                _repository = value;
+            }
+        }
     }
 }
--- ChaosInitiative.Web.ControlPanel/Model/Issue.cs
+++ ChaosInitiative.Web.ControlPanel/Model/Issue.cs
@@ -3,6 +3,11 @@ namespace ChaosInitiative.Web.ControlPanel.Model
     public class Issue
     {
         public Game Game { get; set; }
-        public int IssueId { get; set; }
+        public uint IssueId { get; set; }
+
+        public string GetFullPath()
+        {
+            return $"{Game.Repository}/issues/{IssueId}";
+        }
     }
 }
