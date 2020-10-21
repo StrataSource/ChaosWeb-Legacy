07955b2a0030756e0feea1c2f851fb9e0d8b2b86
Tewan <tewan@spycesoftware.com>
2020-10-21 17:48:30 +0200

    (feat) Make game id auto increment

--- ChaosInitiative.Web.ControlPanel/ApplicationContext.cs
+++ ChaosInitiative.Web.ControlPanel/ApplicationContext.cs
@@ -11,13 +11,13 @@ namespace ChaosInitiative.Web.ControlPanel
             
         }
         
-        public DbSet<Game> Games { get; }
-        public DbSet<Release> Releases { get; }
-        public DbSet<Feature> Features { get; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Release> Releases { get; set; }
+        public DbSet<Feature> Features { get; set;  }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<Game>().Property(g => g.Id).ValueGeneratedOnAdd();
         }
 
     }
