a16b99ab3505862fab393fd156c50afdfeb4b136
Tewan <tewan@spycesoftware.com>
2020-10-28 20:38:36 +0100

    (feat) Enhance control panel games page

--- ChaosInitiative.Web.ControlPanel/Pages/Panel/Games.cshtml.cs
+++ ChaosInitiative.Web.ControlPanel/Pages/Panel/Games.cshtml.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using ChaosInitiative.Web.ControlPanel.Model;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ChaosInitiative.Web.ControlPanel.Pages.Panel
 {
@@ -9,6 +10,7 @@ namespace ChaosInitiative.Web.ControlPanel.Pages.Panel
     {
 
         public List<Game> Games;
+        public List<SelectListItem> AvailableRepoOwners;
 
         private readonly ApplicationContext _context;
 
@@ -20,6 +22,10 @@ namespace ChaosInitiative.Web.ControlPanel.Pages.Panel
         public void OnGet()
         {
             Games = _context.Games.ToList();
+            AvailableRepoOwners = new List<SelectListItem>
+            {
+                new SelectListItem("Chaos Initiative", "ChaosInitiative")
+            };
         }
     }
 }
