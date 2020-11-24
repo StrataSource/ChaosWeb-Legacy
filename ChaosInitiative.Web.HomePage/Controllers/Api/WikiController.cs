7cd99d5c77ad65748c8829bb040a9f7bafe17b07
Tewan <tewan@spycesoftware.com>
2020-11-25 00:21:03 +0100

    feat: Add wiki stuff and removed wiki submodule

---
+++ ChaosInitiative.Web.HomePage/Controllers/Api/WikiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChaosInitiative.Web.HomePage.Controllers.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WikiController : ControllerBase
+    {
+        [HttpPost]
+        [Route("refresh")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult OnRefresh()
+        {
+            WikiUtil.RefreshWikiGitRepository();
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Route("build")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult OnBuild()
+        {
+            WikiUtil.BuildWiki();
+            return NoContent();
+        }
+    }
+}
