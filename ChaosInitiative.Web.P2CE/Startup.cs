770979e8e369527a08c2211c90b154e937aaaad2
Tewan <tewan@spycesoftware.com>
2020-11-28 02:12:03 +0100

    feat: Only use razor runtime compilation in debug mode

--- ChaosInitiative.Web.P2CE/Startup.cs
+++ ChaosInitiative.Web.P2CE/Startup.cs
@@ -21,7 +21,12 @@ namespace ChaosInitiative.Web.P2CE
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddRazorPages()
+#if DEBUG
+                    .AddRazorRuntimeCompilation()
+#endif
+                ;
+            
             services.AddControllers();
             services.AddHostedService<IssueCacheRefreshService>();
         }
