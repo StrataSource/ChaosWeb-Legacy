347bf30f8892185d69e264a983629584ac96c4df
Tewan <tewan@spycesoftware.com>
2021-02-28 23:43:13 +0100

    fix: escape < and > on wiki

--- ChaosInitiative.Web.HomePage/Services/WikiService.cs
+++ ChaosInitiative.Web.HomePage/Services/WikiService.cs
@@ -165,7 +165,9 @@ namespace ChaosInitiative.Web.HomePage.Services
 
         public void ReadMarkdown()
         {
-            MarkdownText = File.ReadAllText(RawPath);
+            MarkdownText = File.ReadAllText(RawPath)
+                               .Replace("<", "&lt;")
+                               .Replace(">", "&gt;");
         }
 
         public void Build()
