9cc80a7ad10987c700d7edf938ed22211553391f
Erin <iansharp1@live.co.uk>
2022-01-16 12:04:48 +0000

    chore: Remove Revolution from the mod list

--- ChaosInitiative.Web.P2CE/Pages/Mods.cshtml.cs
+++ ChaosInitiative.Web.P2CE/Pages/Mods.cshtml.cs
@@ -9,50 +9,11 @@ namespace ChaosInitiative.Web.P2CE.Pages
         
         public void OnGet()
         {
-            // Filling the list manually for now. This can later be hooked up to control panel if needed
+            // Yes this is bad
+            // No we're not changing it just yet
             Mods = new List<Mod>
             {
-                // TODO: Integrate mod list into backend
-                new Mod()
-                {
-                    Name = "revolution",
-                    DisplayName = "Portal: Revolution",
-                    Description = "Portal: Revolution is a mod developed alongside P2CE by Tewan." +
-                                  "The features that the mod could benefit from the most when jumping to Portal 2: Community Edition's engine are " +
-                                  "several graphical enhancements, panorama ui, fixed and improved tools and other small features needed for development.",
-                    Developers = new List<ModDeveloper>
-                    {
-                        new ModDeveloper
-                        {
-                            Name = "Tewan",
-                            Link = "https://twitter.com/spyce_tewan"
-                        },
-                        new ModDeveloper
-                        {
-                            Name = "MysticalAce"
-                        },
-                        new ModDeveloper
-                        {
-                            Name = "ZRezlon"
-                        }
-                    },
-                    Links = new Dictionary<string, string>
-                    {
-                        {
-                            "Steam", 
-                            "https://store.steampowered.com/app/601360/Portal_Revolution/"
-                        },
-                        {
-                            "Discord",
-                            "https://discord.gg/zrSVsM7"
-                        },
-                        {
-                            "ModDB",
-                            "https://www.moddb.com/mods/portal-revolution-spyce-software"
-                        }
-                    }
-                    
-                }
+
             };
         }
     }
