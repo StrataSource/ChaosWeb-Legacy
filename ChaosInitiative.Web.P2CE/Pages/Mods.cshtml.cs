ff9b3a76bdea6926c6ded0c7cd6a1bdb41fca4d6
Erin <iansharp1@live.co.uk>
2022-01-16 13:03:39 +0000

    chore: Add PSC to the mod list

--- ChaosInitiative.Web.P2CE/Pages/Mods.cshtml.cs
+++ ChaosInitiative.Web.P2CE/Pages/Mods.cshtml.cs
@@ -13,7 +13,32 @@ namespace ChaosInitiative.Web.P2CE.Pages
             // No we're not changing it just yet
             Mods = new List<Mod>
             {
-
+                new Mod()
+                {
+                    Name = "psc",
+                    DisplayName = "Portal: Singularity Collapse",
+                    Description = "Long time ago Aperture Science tried to make minature holes for Portal Guns. It turns out that they needed a giant one to make them." +
+                                  "Because GLaDOS killed all of the employees of the facilty, the black hole has become VERY unstable and can suck Earth at any time." +
+                                  "The question is: Will you save Earth and Aperture Science, or use the black hole to your advantage?",
+                    Developers = new List<ModDeveloper>()
+                    {
+                        new ModDeveloper()
+                        {
+                            Name = "Enderek0"
+                        }
+                    },
+                    Links = new Dictionary<string, string>
+                    {
+                        {
+                            "ModDB",
+                            "https://www.moddb.com/mods/portal-singularity-collapse"
+                        },
+                        {
+                            "Discord",
+                            "https://discord.gg/Azv6bfsXS8"
+                        }
+                    }
+                }
             };
         }
     }
