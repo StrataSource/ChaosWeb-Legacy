47f46ebad6d5474c8c9073c7b935d12c6d112d25
Brendan <brendanelewis@gmail.com>
2022-02-23 21:20:13 -0500

    chore: remove more wiki stuff

--- ChaosInitiative.Web.HomePage/Startup.cs
+++ ChaosInitiative.Web.HomePage/Startup.cs
@@ -1,9 +1,7 @@
-using ChaosInitiative.Web.HomePage.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace ChaosInitiative.Web.HomePage
@@ -41,14 +39,6 @@ namespace ChaosInitiative.Web.HomePage
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            
-            // Wiki pages
-            //WikiService wikiService = app.ApplicationServices.GetService<WikiService>();
-            //app.UseStaticFiles(new StaticFileOptions
-            //{
-            //    FileProvider = new PhysicalFileProvider(wikiService.GetWikiOutputPath()),
-            //    RequestPath = "/wiki"
-            //});
 
             app.UseRouting();
 
